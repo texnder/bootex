2425d24c32679b64d245aa584e73f89ff613dd24
Inderjeet <inderjeetchohana1431996@gmail.com>
2020-02-28 22:10:06 +0530

    get full qualified psr4 namespace of each file available in specified directory

---
+++ psr4.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+	/**
+	 | here,
+	 | array's key represents namespace and 
+	 | array's value represents directory path used for the respective namespace
+	 | whichever directory we return here, php files of that directory will be 
+	 | available in bootex service object automaticaly..
+	 */
+
+	"bootex" => __DIR__. "/src"
+
+];
